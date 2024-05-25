a3c76db3dbd095840936790a0fd88874fbb7f220
VenVil835 <venvilots@gmail.com>
2024-05-26 00:13:33 +0800

    UMSSv2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,6 +90,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"
         integrity="sha512-2ImtlRlf2VVmiGZsjm9bEyhjGW4dU7B6TNwh/hx/iSByxNENtj3WVE6o/9Lj4TJeVXPi4bnOIMXFIJJAeufa0A=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="{{ asset('dist/assets/js/alerts.js') }}"></script>
 
     
     @stack('scripts')
