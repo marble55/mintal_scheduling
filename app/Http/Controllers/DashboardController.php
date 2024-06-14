149469dc6aae1de5c971be769f146b53fb703cd8
VenVil835 <venvilots@gmail.com>
2024-06-14 13:58:53 +0800

    main

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Classroom;
 use App\Models\Faculty;
 use App\Models\Schedule;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,6 +19,7 @@ class DashboardController extends Controller
         $all_faculty = Faculty::where('is_part_timer', '=',0)->count();
         $all_partTimer = Faculty::where('is_part_timer', '=',1)->count();
         $total_faculties = Faculty::count();
+        $total_programheads = User::count();
         
         $user = Auth::user();
         return view('dashboard', compact(
@@ -28,6 +30,7 @@ class DashboardController extends Controller
             'all_faculty',
             'all_partTimer',
             'user',
+            'total_programheads',
         ));
     }
 }
--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -1,35 +1,36 @@
 <div class="navbar-container">
     <nav class="navbar">
-        <!-- Navbar Brand Toggle Button (Visible in Mobile View) -->
-        <button class="navbar-toggler" id="navbar-toggler"><i class="lni lni-calendar"></i></button>
 
         <!-- School Year and Semester (Hidden in Mobile View) -->
         <div class="navbar-collapse" id="schoolYearSemester">
             <div class="d-flex flex-column align-items-center">
-                <a class="navbar-brand" style="font-size:18px; color: black;">School Year:
+                <a class="navbar-brand" style=" color: black;">School Year:
                     {{ app('current_academic_year')->getCurrentYearName() }}</a>
-                <span class="navbar-text" style="font-size:15px; color: black;">Semester:
+                <span class="navbar-text" style=" color: black;">Semester:
                     {{ app('current_academic_year')->getCurrentSemesterName() }}</span>
             </div>
         </div>
-
-        <!-- Profile Dropdown -->
-        <div class="profile-dropdown">
-            <a class="profile-link" href="#" id="profileDropdown">
-                <div class="profile-pic">
-                    <img src="{{ app('user_image') }}" alt="Profile Picture">
+        <div class="navbar-right">
+            <!-- Navbar Brand Toggle Button (Visible in Mobile View) -->
+            <button class="navbar-toggler" id="navbar-toggler"><i class="lni lni-calendar"></i></button>
+            <!-- Profile Dropdown -->
+            <div class="profile-dropdown">
+                <a class="profile-link" href="#" id="profileDropdown">
+                    <div class="profile-pic">
+                        <img src="{{ app('user_image') }}" alt="Profile Picture">
+                    </div>
+                </a>
+                <div class="dropdown-menu" id="dropdownMenu">
+                    <a class="dropdown-item" href="{{ route('profile.edit') }}">Account</a>
+                    <hr class="dropdown-divider">
+                    <!-- Logout Form -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit" class="dropdown-item text-danger" onclick="event.preventDefault(); this.closest('form').submit();">
+                            Logout
+                        </button>
+                    </form>
                 </div>
-            </a>
-            <div class="dropdown-menu" id="dropdownMenu">
-                <a class="dropdown-item" href="{{ route('profile.edit') }}">Account</a>
-                <hr class="dropdown-divider">
-                <!-- Logout Form -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button type="submit" class="dropdown-item text-danger" onclick="event.preventDefault(); this.closest('form').submit();">
-                        Logout
-                    </button>
-                </form>
             </div>
         </div>
     </nav>
